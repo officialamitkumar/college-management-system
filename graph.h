6e66f6e30f818db096e1e460fa61b86d2052c2ff
Amit KUMAR <55083648+officialamitkumar@users.noreply.github.com>
2021-09-07 00:13:45 +0530

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,272 @@
+#include<graphics.h>
+
+
+  void dora()
+	  {
+
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(1,1,RED);
+		setcolor(0);
+
+
+		arc(250,116,-43,225,110);
+
+		arc(250,-100,-106,-74,300);
+		arc(250,-90,-106,-74,300);
+		arc(188,193,160,200,20);
+		arc(312,191,-25,10,20);
+
+
+		ellipse(230,85,0,360,20,25);
+		ellipse(270,85,0,360,20,25);
+
+
+
+		ellipse(240,90,0,360,3,7);
+		ellipse(260,90,0,360,3,7);
+
+
+		circle(250,110,10);
+
+
+
+		arc(260,160,124,201,90);
+		arc(240,160,-20,56,90);
+
+
+		ellipse(252,140,190,350,50,50);
+
+		arc(252,290,70,110,150);
+
+
+		arc(10,284,-10,15,330);
+		arc(490,283,165,190,330);
+
+
+
+		line(250,120,250,140);
+
+		line(260,120,300,115);
+		line(260,125,300,125);
+		line(260,130,300,135);
+
+
+
+		line(200,115,240,120);
+		line(200,125,240,125);
+		line(200,135,240,130);
+
+
+
+
+		arc(193,11,-95,-82,330);
+		arc(308,13,-98,-85,330);
+
+
+		arc(250,250,-222,41,70);
+
+
+		ellipse(250,335,-10,190,13,15);
+
+		ellipse(198,350,136,392,45,25);
+		ellipse(300,350,143,399,45,25);
+
+
+
+		 arc(286,92,-70,-45,130);
+		 circle(385,165,20);
+		 arc(420,283,111,125,135);
+
+
+		arc(238,300,120,155,130);
+		circle(120,265,20);
+		line(138,265,163,245);
+
+
+		ellipse(250,250,180,360,40,45);
+		line(210,250,290,250);
+
+
+		circle(250,216,13);
+
+
+
+
+
+		setfillstyle(SOLID_FILL,BLUE);
+		floodfill(250,30,0);
+		floodfill(350,170,0);
+		floodfill(140,245,0);
+
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(285,130,0);
+
+		setfillstyle(SOLID_FILL,RED);
+		floodfill(250,110,0);
+
+		setfillstyle(SOLID_FILL,RED);
+		floodfill(285,165,0);
+
+
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(230,65,0);
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(260,65,0);
+
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(250,99,0);
+
+		setfillstyle(SOLID_FILL,BLUE);
+		floodfill(320,284,0);
+		floodfill(180,283,0);
+
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(200,283,0);
+		floodfill(250,260,0);
+		floodfill(385,165,0);
+		floodfill(120,265,0);
+		floodfill(198,350,0);
+		floodfill(300,350,0);
+
+		setfillstyle(SOLID_FILL,YELLOW);
+		floodfill(250,216,0);
+		floodfill(250,206,0);
+
+
+		setfillstyle(SOLID_FILL,RED);
+		floodfill(235,206,0);
+
+
+
+		setfillstyle(SOLID_FILL,0);
+		floodfill(240,90,0);
+
+		setfillstyle(SOLID_FILL,0);
+		floodfill(260,90,0);
+
+
+
+	   }
+
+
+	   void mick()
+	   {
+
+		setcolor(0);
+
+		setfillstyle(SOLID_FILL,WHITE);
+		floodfill(10,10,RED);
+
+
+
+		arc(200,150,0,260,60);
+		arc(285,246,75,105,100);
+		arc(370,150,-79,180,60);
+
+		//setfillstyle(SOLID_FILL,WHITE);
+		//floodfill(316,242,WHITE);
+
+
+		arc(316,242,164,202,130);
+		arc(256,243,-21,16,130);
+
+
+		arc(207,296,88,192,15);
+		arc(363,295,-28,105,15);
+
+
+
+		arc(290,282,190,245,100);
+		arc(277,268,-70,-18,105);
+		arc(279,358,205,348,35);
+
+
+		arc(248,190,0,174,35);
+		arc(317,190,4,180,35);
+
+
+		//setfillstyle(SOLID_FILL,RED);
+		//floodfill(220,180,WHITE);
+		arc(335,242,153,198,135);
+		arc(220,242,-15,26,145);
+
+
+		arc(285,320,55,125,60);
+
+		//setcolor(RED);
+		ellipse(260,235,-48,250,15,35);
+		ellipse(310,235,-70,235,15,35);
+
+
+		arc(285,260,-150,-30,70);
+
+		ellipse(280,295,-167,-20,45,70);
+
+		arc(240,305,120,165,20);
+		arc(328,305,10,60,20);
+
+
+		arc(263,360,30,130,20);
+		arc(290,360,30,130,20);
+
+		//setcolor(RED);
+		ellipse(270,253,30,250,5,10);
+		ellipse(299,255,-32,110,5,10);
+
+
+
+		ellipse(285,290,0,360,30,20);
+
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(285,290,0);
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(200,150,0);
+
+
+		setfillstyle(SOLID_FILL,RED);
+		floodfill(263,350,0);
+
+		//setfillstyle(SOLID_FILL,WHITE);
+		//floodfill(250,290,WHITE);
+
+
+		//setfillstyle(SOLID_FILL,WHITE);
+		//floodfill(260,235,RED);
+
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(263,335,0);
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(370,150,0);
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(285,148,0);
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(270,253,0);
+
+
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(299,255,0);
+
+	   }
+
+
+
+
+
